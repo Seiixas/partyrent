b74630e0cd033164f5e13e01f82f3646f7c68e12
Mateus Seixas <mateus.dev@hublocal.com.br>
2023-07-15 16:00:03 -0300

    ✨Initial commit

---
+++ src/main/java/br/ifnmg/edu/partyrent/PartyrentApplication.java
@@ -0,0 +1,13 @@
+package br.ifnmg.edu.partyrent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartyrentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PartyrentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/ifnmg/edu/partyrent/PartyrentApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ifnmg.edu.partyrent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PartyrentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
